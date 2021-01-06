c3fc7157d4ecf29f32da52a468f3c138ae075669
yniejun <y.niejun@gmail.com>
2021-01-06 16:28:54 +0000

    poker game deal card

--- src/com/examples/Demo.java
+++ src/com/examples/Demo.java
@@ -10,9 +10,7 @@ public class Demo {
         ArrayList<String>[] card = p.dealCard(number);
         for (int i = 0; i < number; i++) {
             System.out.println("Member" + i + ":");
-            for (String str : card[i]) {
-                System.out.println(str);
-            }
+            System.out.println(card[i]);
             System.out.println("");
         }
         System.out.println("unreleased card \n" + card[number]);
